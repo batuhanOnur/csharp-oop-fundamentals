f84435bc37df2af541c197a226fa61dfd4a1917d
batuhanOnur <batuhanonur29@gmail.com>
2021-04-22 16:53:57 +0300

    Building Entity Classes

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        private string _lastName; //backing field
+        public string LastName  
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+        public static int InstanceCount { get; set; }  // class'a aittir.
+
+        public bool Validate()
+        {
+            var isValid = true;
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
+        }
+    }
+}
---
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,81 @@
+﻿using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //--Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Batuhan",
+                LastName = "Onur"
+            };
+            string expected = "Onur, Batuhan";
+
+            //--Act
+            string actual = customer.FullName;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FullNameFirstEmpty()
+        {
+            //--Arrange
+            Customer customer = new Customer
+            {
+                LastName = "Onur"
+            };
+            string expected = "Onur";
+
+            //--Act
+            string actual = customer.FullName;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //--Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Batuhan"
+            };
+            string expected = "Batuhan";
+
+            //--Act
+            string actual = customer.FullName;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void StaticTest()
+        {
+            //--Arrange
+            var c1 = new Customer();
+            c1.FirstName = "Bilbo";  // c1 InstanceCount'a ulaşamaz. çünkü static.
+            Customer.InstanceCount += 1;  // static'e ulaşmak için direk object çağırılır.
+
+            var c2 = new Customer();
+            c2.FirstName = "Frodo";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Rosie";
+            Customer.InstanceCount += 1;
+
+            //--Act
+
+            //--Assert
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
+    }
+}
